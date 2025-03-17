4e8e4e61cb43a4d1cbca793889048f3f5751e6de
tyVespA <migliavacca.marco08@gmail.com>
2025-03-17 11:47:52 +0100

    add auth to all routes

--- index.js
+++ index.js
@@ -1,68 +1,72 @@
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const authenticateUser = require("./middleware/auth");
+const authRoutes = require("./auth/firebase/authTest");
 const express = require("express");
 const cors = require("cors");
 const Weight = require("./models/weight");
 
+const allowedOrigins = ["http://localhost:5173", "deployed-link"];
+
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
 app.use(express.static("dist"));
 
-app.get("/api/weights", (req, res) => {
-  Weight.find({}).then((weights) => {
+app.use("/", authRoutes);
+
+app.get("/api/weights", authenticateUser, (req, res) => {
+  Weight.find({ userId: req.user.id }).then((weights) => {
     res.json(weights);
   });
 });
 
-app.get("/api/weights/:id", (req, res, next) => {
-  Weight.findById(req.params.id)
-    .then((weight) => {
-      if (weight) {
-        res.json(weight);
-      } else {
-        res.status(404).end();
-      }
-    })
-    .catch((err) => next(err));
-});
-
-app.delete("/api/weights/:id", (req, res, next) => {
-  Weight.findByIdAndDelete(req.params.id)
-    .then((result) => {
-      res.status(204).end();
-    })
-    .catch((err) => next(err));
-});
-
-app.put("/api/weights/:id", (req, res, next) => {
+app.post("/api/weights", authenticateUser, (req, res, next) => {
   const { weight } = req.body;
 
-  Weight.findByIdAndUpdate(
-    req.params.id,
-    { weight },
+  const newWeight = new Weight({
+    weight: Number(weight),
+    userId: req.user.id,
+  });
+
+  newWeight
+    .save()
+    .then((savedWeight) => res.json(savedWeight))
+    .catch((err) => next(err));
+});
+
+app.get("/api/weights/:id", authenticateUser, (req, res, next) => {
+  Weight.findOne({ _id: req.params.id, userId: req.user.id })
+    .then((weight) => {
+      if (weight) res.json(weight);
+      else res.status(404).json({ message: "Not found" });
+    })
+    .catch((err) => next(err));
+});
+
+app.put("/api/weights/:id", authenticateUser, (req, res, next) => {
+  Weight.findOneAndUpdate(
+    { _id: req.params.id, userId: req.user.id },
+    { weight: req.body.weight },
     { new: true, runValidators: true, context: "query" }
   )
     .then((updatedWeight) => {
-      if (updatedWeight) {
-        res.json(updatedWeight);
-      } else {
-        res.status(404).end;
-      }
+      if (updatedWeight) res.json(updatedWeight);
+      else res.status(404).json({ message: "Not found" });
     })
     .catch((err) => next(err));
 });
 
-app.post("/api/weights", (req, res, next) => {
-  const body = req.body;
-
-  const weight = new Weight({
-    weight: Number(body.weight),
-  });
-
-  weight
-    .save()
-    .then((savedWeight) => {
-      res.json(savedWeight);
+app.delete("/api/weights/:id", authenticateUser, (req, res, next) => {
+  Weight.findOneAndDelete({ _id: req.params.id, userId: req.user.id })
+    .then((result) => {
+      if (result) res.status(204).end();
+      else res.status(404).json({ message: "Not found" });
     })
     .catch((err) => next(err));
 });
