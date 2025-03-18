2d01f2239f6104d97445c0d3a4024e97a6ae01aa
tyVespA <migliavacca.marco08@gmail.com>
2025-03-18 15:07:12 +0100

    fix cors

--- index.js
+++ index.js
@@ -8,18 +8,26 @@ const Weight = require("./models/weight");
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://traccia-peso.vercel.app/",
+  "https://traccia-peso.vercel.app",
 ];
 
+const corsOptions = {
+  origin: allowedOrigins,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
 const app = express();
+
 app.use(express.json());
-app.use(
-  cors({
-    origin: allowedOrigins,
-    credentials: true,
-  })
-);
+app.use(cors(corsOptions));
 app.use(express.static("dist"));
+app.use((req, res, next) => {
+  res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+  res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+  next();
+});
 
 app.use("/", authRoutes);
 
