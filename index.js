a8a74bd0e5e6a8348d4194e4f3c34f615888c59f
tyVespA <migliavacca.marco08@gmail.com>
2025-03-18 14:22:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ const errorHandler = (err, req, res, next) => {
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log("App running on port " + PORT);
 });
